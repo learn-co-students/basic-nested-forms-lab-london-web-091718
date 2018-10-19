76f51b5fddd396f58a89038b5acb84826bcd1eb3
Amalie Smidth <amaliesmidth@gmail.com>
2018-10-19 14:23:45 +0100

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+rec1 = Recipe.create(title: "Delish Recipe")
+rec2 = Recipe.create(title: "Groos Rec")
+
+apple = Ingredient.create(name: "Apple", quantity: "10", recipe: rec1)
+strawb = Ingredient.create(name: "Strawberry", quantity: "1", recipe: rec1)
+banana = Ingredient.create(name: "Banana", quantity: "200", recipe: rec2)
+rasp = Ingredient.create(name: "Raspberry", quantity: "44", recipe: rec2)
